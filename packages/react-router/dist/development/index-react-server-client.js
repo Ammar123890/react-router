d109d51c8196455c15cdbb7b676970794bcfc869
Syed Muhammad Ammar <muhammad.ammar@dubizzlelabs.com>
2026-02-22 15:08:37 +0500

    Include build artifacts for fix

---
+++ packages/react-router/dist/development/index-react-server-client.js
@@ -0,0 +1,61 @@
+"use strict";Object.defineProperty(exports, "__esModule", {value: true});/**
+ * react-router v7.13.0
+ *
+ * Copyright (c) Remix Software Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.md file in the root directory of this source tree.
+ *
+ * @license MIT
+ */
+"use client";
+
+
+
+
+
+
+
+
+
+
+var _chunkQT6NESM4js = require('./chunk-QT6NESM4.js');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var _chunkY7QT52CYjs = require('./chunk-Y7QT52CY.js');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+exports.BrowserRouter = _chunkQT6NESM4js.BrowserRouter; exports.Form = _chunkQT6NESM4js.Form; exports.HashRouter = _chunkQT6NESM4js.HashRouter; exports.Link = _chunkQT6NESM4js.Link; exports.Links = _chunkY7QT52CYjs.Links; exports.MemoryRouter = _chunkY7QT52CYjs.MemoryRouter; exports.Meta = _chunkY7QT52CYjs.Meta; exports.NavLink = _chunkQT6NESM4js.NavLink; exports.Navigate = _chunkY7QT52CYjs.Navigate; exports.Outlet = _chunkY7QT52CYjs.Outlet; exports.Route = _chunkY7QT52CYjs.Route; exports.Router = _chunkY7QT52CYjs.Router; exports.RouterProvider = _chunkY7QT52CYjs.RouterProvider; exports.Routes = _chunkY7QT52CYjs.Routes; exports.ScrollRestoration = _chunkQT6NESM4js.ScrollRestoration; exports.StaticRouter = _chunkQT6NESM4js.StaticRouter; exports.StaticRouterProvider = _chunkQT6NESM4js.StaticRouterProvider; exports.UNSAFE_AwaitContextProvider = _chunkY7QT52CYjs.AwaitContextProvider; exports.UNSAFE_WithComponentProps = _chunkY7QT52CYjs.WithComponentProps; exports.UNSAFE_WithErrorBoundaryProps = _chunkY7QT52CYjs.WithErrorBoundaryProps; exports.UNSAFE_WithHydrateFallbackProps = _chunkY7QT52CYjs.WithHydrateFallbackProps; exports.unstable_HistoryRouter = _chunkQT6NESM4js.HistoryRouter;
---
+++ packages/react-router/dist/production/index-react-server-client.js
@@ -0,0 +1,61 @@
+"use strict";Object.defineProperty(exports, "__esModule", {value: true});/**
+ * react-router v7.13.0
+ *
+ * Copyright (c) Remix Software Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.md file in the root directory of this source tree.
+ *
+ * @license MIT
+ */
+"use client";
+
+
+
+
+
+
+
+
+
+
+var _chunkLCBSZLIOjs = require('./chunk-LCBSZLIO.js');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var _chunk5XHXSQOYjs = require('./chunk-5XHXSQOY.js');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+exports.BrowserRouter = _chunkLCBSZLIOjs.BrowserRouter; exports.Form = _chunkLCBSZLIOjs.Form; exports.HashRouter = _chunkLCBSZLIOjs.HashRouter; exports.Link = _chunkLCBSZLIOjs.Link; exports.Links = _chunk5XHXSQOYjs.Links; exports.MemoryRouter = _chunk5XHXSQOYjs.MemoryRouter; exports.Meta = _chunk5XHXSQOYjs.Meta; exports.NavLink = _chunkLCBSZLIOjs.NavLink; exports.Navigate = _chunk5XHXSQOYjs.Navigate; exports.Outlet = _chunk5XHXSQOYjs.Outlet; exports.Route = _chunk5XHXSQOYjs.Route; exports.Router = _chunk5XHXSQOYjs.Router; exports.RouterProvider = _chunk5XHXSQOYjs.RouterProvider; exports.Routes = _chunk5XHXSQOYjs.Routes; exports.ScrollRestoration = _chunkLCBSZLIOjs.ScrollRestoration; exports.StaticRouter = _chunkLCBSZLIOjs.StaticRouter; exports.StaticRouterProvider = _chunkLCBSZLIOjs.StaticRouterProvider; exports.UNSAFE_AwaitContextProvider = _chunk5XHXSQOYjs.AwaitContextProvider; exports.UNSAFE_WithComponentProps = _chunk5XHXSQOYjs.WithComponentProps; exports.UNSAFE_WithErrorBoundaryProps = _chunk5XHXSQOYjs.WithErrorBoundaryProps; exports.UNSAFE_WithHydrateFallbackProps = _chunk5XHXSQOYjs.WithHydrateFallbackProps; exports.unstable_HistoryRouter = _chunkLCBSZLIOjs.HistoryRouter;
